d010e9a3e095deb0745ab92932b125c1cd74d448
Huy <seuljanvier@gmail.com>
2024-06-24 18:13:17 +0300

    fix next.name bug

--- src/PokemonPage.jsx
+++ src/PokemonPage.jsx
@@ -35,12 +35,13 @@ const PokemonPage = ({ previous, next }) => {
   )
 
   // console.log('hiddenAbility=', hiddenAbility)
+
   return (
     <>
       <div className="links">
         {previous && <Link to={`/pokemon/${previous.name}`}>Previous</Link>}
         <Link to="/">Home</Link>
-        {next && <Link to={`/pokemon/${previous.name}`}>Next</Link>}
+        {next && <Link to={`/pokemon/${next.name}`}>Next</Link>}
       </div>
       <div className={`pokemon-page pokemon-type-${type.name}`}>
         <div
